a2220af301c1a3dc9f92458efb8b7593c4359acb
LouiseDavise <louizkwok@gmail.com>
2025-06-04 18:21:26 +0800

    Player Moving Animation

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -1,22 +1,78 @@
 #include "Player.hpp"
+#include "Engine/Resources.hpp"
+#include <allegro5/allegro_primitives.h>
+#include <cmath>
 
 Player::Player(float x, float y)
-    : Engine::Sprite("play/enemy-4.png", x, y), moveSpeed(200.0f), keyW(false), keyA(false), keyS(false), keyD(false) {
+    : Engine::Sprite("play/player_1/image1x1.png", x, y),
+      moveSpeed(200.0f), keyW(false), keyA(false), keyS(false), keyD(false),
+      faceRight(true), animationTimer(0), currentFrame(0), animationInterval(0.1f) {
     Anchor = Engine::Point(0.5f, 0.5f);
+
+    for (int i = 1; i <= 7; ++i) {
+        std::string path = "play/player_1/image" + std::to_string(i) + "x1.png";
+        animationsRight.push_back(Engine::Resources::GetInstance().GetBitmap(path));
+    }
+    currentBitmap = animationsRight[0];
 }
 
-void Player::Update(float deltaTime) {  
+void Player::Update(float deltaTime) {
     Engine::Point velocity(0, 0);
     if (keyW) velocity.y -= 1;
     if (keyS) velocity.y += 1;
-    if (keyA) velocity.x -= 1;
-    if (keyD) velocity.x += 1;
-    if (velocity.x != 0 || velocity.y != 0) {
+    if (keyA) {
+        velocity.x -= 1;
+        faceRight = false;
+    }
+    if (keyD) {
+        velocity.x += 1;
+        faceRight = true;
+    }
+
+    bool moving = (velocity.x != 0 || velocity.y != 0);
+    bool movingHorizontally = (velocity.x != 0);
+
+    if (moving) {
         float len = std::sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
         Position.x += (velocity.x / len) * moveSpeed * deltaTime;
         Position.y += (velocity.y / len) * moveSpeed * deltaTime;
     }
+
+    UpdateAnimation(deltaTime, movingHorizontally);
     Engine::Sprite::Update(deltaTime);
+}
+
+void Player::UpdateAnimation(float deltaTime, bool movingHorizontally) {
+    if (!movingHorizontally) return;
+    animationTimer += deltaTime;
+    if (animationTimer >= animationInterval) {
+        animationTimer = 0;
+        currentFrame = (currentFrame + 1) % animationsRight.size();
+        currentBitmap = animationsRight[currentFrame];
+    }
+}
+
+void Player::Draw() const {
+    if (!currentBitmap) return;
+
+    ALLEGRO_BITMAP* bmp = currentBitmap.get();
+    float bmpW = al_get_bitmap_width(bmp);
+    float bmpH = al_get_bitmap_height(bmp);
+    float scaleX = faceRight ? 1.0f : -1.0f;
+    float anchorOffsetX = faceRight ? Anchor.x : (1.0f - Anchor.x);
+
+    al_draw_tinted_scaled_rotated_bitmap(
+        bmp,
+        Tint,
+        anchorOffsetX * bmpW,
+        Anchor.y * bmpH,
+        Position.x,
+        Position.y,
+        scaleX * (Size.x / bmpW),
+        Size.y / bmpH,
+        Rotation,
+        0
+    );
 }
 
 void Player::OnKeyDown(int keyCode) {
--- Player/Player.hpp
+++ Player/Player.hpp
@@ -3,18 +3,29 @@
 
 #include "Engine/Sprite.hpp"
 #include "Engine/Point.hpp"
-#include <allegro5/allegro.h>
+#include <vector>
+#include <memory>
 
 class Player final : public Engine::Sprite {
 public:
     float moveSpeed;
     Player(float x, float y);
     void Update(float deltaTime) override;
+    void Draw() const override;
     void OnKeyDown(int keyCode);
     void OnKeyUp(int keyCode);
 
 private:
     bool keyW, keyA, keyS, keyD;
+    bool faceRight;
+    float animationTimer;
+    int currentFrame;
+    float animationInterval;
+
+    std::vector<std::shared_ptr<ALLEGRO_BITMAP>> animationsRight;
+    std::shared_ptr<ALLEGRO_BITMAP> currentBitmap;
+
+    void UpdateAnimation(float deltaTime, bool movingHorizontally);
 };
 
-#endif // PLAYER_HPP
+#endif
