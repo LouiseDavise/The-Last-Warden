2f4680dd6b32ed1c256b97ba52015d32ed5f7ca8
LouiseDavise <louizkwok@gmail.com>
2025-06-14 04:15:53 +0800

    Change SelectCompanionScene

--- Scene/SelectCompanionScene.cpp
+++ Scene/SelectCompanionScene.cpp
@@ -23,41 +23,61 @@ void SelectCompanionScene::Initialize()
     const int startX = halfW - spacing - 70;
     const int compY = 400;
 
-    std::string compFolders[] = {"Support-1", "Support-2", "Support-3"};
-    std::string compNames[] = {"Wisp", "Mochi", "Zuko"};
+    struct CompanionOption
+    {
+        std::string name;
+        std::string desc1;
+        std::string desc2;
+        std::string imagePath;
+    };
+
+    CompanionOption companions[count] = {
+        {"Wisp", "Swaps place with player", "", "Characters/Support-1/image1x1.png"},
+        {"Mochi", "Heals player nearby & unlimited", "attack within range", "Characters/Support-2/image1x1.png"},
+        {"Zuko", "Knocks back enemies", "", "Characters/Support-3/image1x1.png"}};
 
     for (int i = 0; i < count; ++i)
     {
         int x = startX + i * spacing;
 
-        AddNewObject(new Engine::Image("Characters/" + compFolders[i] + "/image1x1.png", x, compY, 150, 150));
-        AddNewObject(new Engine::Label(compNames[i], "pirulen.ttf", 28, x + 75, compY + 160, 255, 255, 255, 255, 0.5, 0.0));
-        if(i == 0)AddNewObject(new Engine::Label("Unique Soul", "pirulen.ttf", 18, x + 75, compY + 200, 200, 200, 200, 255, 0.5, 0.0));
-        if(i == 1)AddNewObject(new Engine::Label("Sharp Sidekick", "pirulen.ttf", 18, x + 75, compY + 200, 200, 200, 200, 255, 0.5, 0.0));
-        if(i == 2)AddNewObject(new Engine::Label("Bold Adventurer", "pirulen.ttf", 18, x + 75, compY + 200, 200, 200, 200, 255, 0.5, 0.0));
+        // Image
+        AddNewObject(new Engine::Image(companions[i].imagePath, x, compY, 150, 150));
 
+        // Companion name
+        AddNewObject(new Engine::Label(companions[i].name, "pirulen.ttf", 28, x + 75, compY + 160, 255, 255, 255, 255, 0.5, 0.0));
+
+        // Description lines
+        AddNewObject(new Engine::Label(companions[i].desc1, "pirulen.ttf", 12, x + 75, compY + 200, 200, 200, 200, 255, 0.5, 0.0));
+        if (!companions[i].desc2.empty())
+            AddNewObject(new Engine::Label(companions[i].desc2, "pirulen.ttf", 12, x + 75, compY + 225, 200, 200, 200, 255, 0.5, 0.0));
+
+        // Select button
         auto *btn = new Engine::ImageButton("UI/button.png", "UI/button-transparant.png", x + 10, compY + 250, 130, 60);
         btn->SetOnClickCallback(std::bind(&SelectCompanionScene::OnSelectClick, this, i));
         AddNewControlObject(btn);
         AddNewObject(new Engine::Label("Select", "RealwoodRegular.otf", 28, x + 75, compY + 280, 255, 255, 255, 255, 0.5, 0.5));
     }
 
-    // Confirm Box
+    // Confirmation box
     confirmBox = new Engine::Image("UI/confirm-box.png", halfW - 300, 350, 600, 400);
     confirmBox->Visible = false;
     AddNewObject(confirmBox);
 
-    confirmText = new Engine::Label("", "RealwoodRegular.otf", 38, halfW, 460, 255, 255, 255, 255, 0.5, 0.5);
+    // Confirmation text
+    confirmText = new Engine::Label("", "RealwoodRegular.otf", 28, halfW, 460, 255, 255, 255, 255, 0.5, 0.5);
     confirmText->Visible = false;
     AddNewObject(confirmText);
 
-    warningText1 = new Engine::Label("Prepare for battle alongside", "RealwoodRegular.otf", 26, halfW, 525, 255, 100, 100, 255, 0.5, 0.5);
+    // Warning text
+    warningText1 = new Engine::Label("Prepare for battle alongside", "RealwoodRegular.otf", 20, halfW, 525, 255, 100, 100, 255, 0.5, 0.5);
     warningText1->Visible = false;
     AddNewObject(warningText1);
-    warningText2 = new Engine::Label("", "RealwoodRegular.otf", 26, halfW, 550, 255, 100, 100, 255, 0.5, 0.5);
+
+    warningText2 = new Engine::Label("", "RealwoodRegular.otf", 20, halfW, 545, 255, 100, 100, 255, 0.5, 0.5);
     warningText2->Visible = false;
     AddNewObject(warningText2);
 
+    // Confirm button
     confirmBtn = new Engine::ImageButton("UI/button.png", "UI/button-transparant.png", halfW - 160, 600, 140, 65);
     confirmBtn->SetOnClickCallback([this]()
                                    {
@@ -67,10 +87,12 @@ void SelectCompanionScene::Initialize()
         Engine::GameEngine::GetInstance().ChangeScene("play"); });
     confirmBtn->Visible = false;
     AddNewControlObject(confirmBtn);
+
     confirmLabel = new Engine::Label("Confirm", "pirulen.ttf", 17, halfW - 90, 630, 255, 255, 255, 255, 0.5, 0.5);
     confirmLabel->Visible = false;
     AddNewObject(confirmLabel);
 
+    // Cancel button
     cancelBtn = new Engine::ImageButton("UI/button.png", "UI/button-transparant.png", halfW + 40, 600, 140, 65);
     cancelBtn->SetOnClickCallback([this]()
                                   {
@@ -84,15 +106,17 @@ void SelectCompanionScene::Initialize()
         cancelLabel->Visible = false; });
     cancelBtn->Visible = false;
     AddNewControlObject(cancelBtn);
+
     cancelLabel = new Engine::Label("Cancel", "pirulen.ttf", 17, halfW + 110, 630, 255, 255, 255, 255, 0.5, 0.5);
     cancelLabel->Visible = false;
     AddNewObject(cancelLabel);
 
-    auto backButton = new Engine::ImageButton("UI/button.png", "UI/button-transparant.png", halfW - 240, 790, 480, 115);
+    // Back Button
+    auto *backButton = new Engine::ImageButton("UI/button.png", "UI/button-transparant.png", halfW - 240, 790, 480, 115);
     backButton->SetOnClickCallback([]()
                                    { Engine::GameEngine::GetInstance().ChangeScene("mode-select"); });
     AddNewControlObject(backButton);
-    AddNewObject(new Engine::Label("BACK", "RealwoodRegular.otf", 56, halfW, 800 + 115 / 2, 255, 255, 255, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("BACK", "RealwoodRegular.otf", 56, halfW, 847, 255, 255, 255, 255, 0.5, 0.5));
 }
 
 void SelectCompanionScene::OnSelectClick(int compId)
